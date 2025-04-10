485de51df540909b146ed6b7aaf72e059f094eef
Luiz Otávio <luizomf@gmail.com>
2025-04-10 10:10:51 -0300

    Sobre o Tailwind CSS, clsx e extensões do VS Code para facilitar sua vida

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
+import clsx from 'clsx';
+
 export default function HomePage() {
-  return <h1>Olá de dentro de page.tsx</h1>;
+  return (
+    <div>
+      <h1
+        className={clsx(
+          'text-6xl',
+          'font-bold',
+          'text-blue-500',
+          'hover:text-blue-50',
+          'hover:bg-blue-500',
+          'transition',
+          'duration-1000',
+        )}
+      >
+        Texto no meu h1
+      </h1>
+    </div>
+  );
 }
