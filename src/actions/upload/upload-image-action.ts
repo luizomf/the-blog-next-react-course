45c58b0623b725e087873e7b8796b3a65b8134b0
Luiz Otávio <luizomf@gmail.com>
2025-04-17 12:57:35 -0300

    Salvando a imagem enviada dentro do servidor com JS + Node

--- src/actions/upload/upload-image-action.ts
+++ src/actions/upload/upload-image-action.ts
@@ -1,6 +1,12 @@
 'use server';
 
-import { IMAGE_UPLOAD_MAX_SIZE } from '@/lib/constants';
+import {
+  IMAGE_SERVER_URL,
+  IMAGE_UPLOAD_DIRECTORY,
+  IMAGE_UPLOAD_MAX_SIZE,
+} from '@/lib/constants';
+import { mkdir, writeFile } from 'fs/promises';
+import { extname, resolve } from 'path';
 
 type UploadImageActionResult = {
   url: string;
@@ -10,6 +16,8 @@ type UploadImageActionResult = {
 export async function uploadImageAction(
   formData: FormData,
 ): Promise<UploadImageActionResult> {
+  // TODO: Verificar se o usuário está logado
+
   const makeResult = ({ url = '', error = '' }) => ({ url, error });
 
   if (!(formData instanceof FormData)) {
@@ -30,6 +38,24 @@ export async function uploadImageAction(
     return makeResult({ error: 'Imagem inválida' });
   }
 
-  // TODO: enviei o arquivo
-  return makeResult({ url: 'URL' });
+  const imageExtension = extname(file.name);
+  const uniqueImageName = `${Date.now()}${imageExtension}`;
+
+  const uploadFullPath = resolve(
+    process.cwd(),
+    'public',
+    IMAGE_UPLOAD_DIRECTORY,
+  );
+  await mkdir(uploadFullPath, { recursive: true });
+
+  const fileArrayBuffer = await file.arrayBuffer();
+  const buffer = Buffer.from(fileArrayBuffer);
+
+  const fileFullPath = resolve(uploadFullPath, uniqueImageName);
+
+  await writeFile(fileFullPath, buffer);
+
+  const url = `${IMAGE_SERVER_URL}/${uniqueImageName}`;
+
+  return makeResult({ url });
 }
