a916903b11c8d46dd0123ca9f2513534a22a6050
Luiz Otávio <luizomf@gmail.com>
2025-04-19 05:53:46 -0300

    Ajustes finais no formulário + server action updatePostAction

--- src/components/admin/ImageUploader/index.tsx
+++ src/components/admin/ImageUploader/index.tsx
@@ -7,7 +7,11 @@ import { ImageUpIcon } from 'lucide-react';
 import { useRef, useState, useTransition } from 'react';
 import { toast } from 'react-toastify';
 
-export function ImageUploader() {
+type ImageUploaderProps = {
+  disabled?: boolean;
+};
+
+export function ImageUploader({ disabled = false }: ImageUploaderProps) {
   const fileInputRef = useRef<HTMLInputElement>(null);
   const [isUploading, startTransition] = useTransition();
   const [imgUrl, setImgUrl] = useState('');
@@ -69,7 +73,7 @@ export function ImageUploader() {
         onClick={handleChooseFile}
         type='button'
         className='self-start'
-        disabled={isUploading}
+        disabled={isUploading || disabled}
       >
         <ImageUpIcon />
         Enviar uma imagem
@@ -93,7 +97,7 @@ export function ImageUploader() {
         name='file'
         type='file'
         accept='image/*'
-        disabled={isUploading}
+        disabled={isUploading || disabled}
       />
     </div>
   );
