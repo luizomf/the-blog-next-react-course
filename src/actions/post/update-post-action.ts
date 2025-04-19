17a1c86b5efe5a415642c59970a235f6b71bdb4a
Luiz Otávio <luizomf@gmail.com>
2025-04-19 09:27:57 -0300

    Revisão, limpeza e pequenas correções no código

--- src/actions/post/update-post-action.ts
+++ src/actions/post/update-post-action.ts
@@ -7,7 +7,6 @@ import {
 } from '@/dto/post/dto';
 import { PostUpdateSchema } from '@/lib/post/validations';
 import { postRepository } from '@/repositories/post';
-import { asyncDelay } from '@/utils/async-delay';
 import { getZodErrorMessages } from '@/utils/get-zod-error-messages';
 import { makeRandomString } from '@/utils/make-random-string';
 import { revalidateTag } from 'next/cache';
@@ -23,8 +22,6 @@ export async function updatePostAction(
   formData: FormData,
 ): Promise<UpdatePostActionState> {
   // TODO: verificar se o usuário tá logado
-
-  await asyncDelay(3000);
 
   if (!(formData instanceof FormData)) {
     return {
